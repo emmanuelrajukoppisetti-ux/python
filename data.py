326a1421bbefb12d4495886f452040250031cda7
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-11 20:55:23 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+'''import mymodule
+mymodule.greetings("emmanuel")'''
+
+
+#import mymodule
+
+
+'''import mymodule
+a=mymodule.Details["idnos"]
+b=mymodule.Details["names"]
+c=mymodule.Details["cities"]
+print(a,b,c)'''
+
+
+#import mymodule
