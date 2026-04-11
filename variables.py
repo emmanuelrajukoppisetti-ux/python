ed10655c2290730bfdc45cf50afe29dc32f69405
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-11 20:47:45 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,86 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(4+7)
+11
+a=2
+print(a)
+2
+b=5print
+SyntaxError: invalid decimal literal
+b=5
+print(b)
+5
+c=100
+print(c)
+100
+print(x)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+x=20
+print(x)
+20
+z=50
+print(z)
+50
+a0123456789=200
+print(a0123456789)
+200
+a2=9
+print(a2)
+9
+2a=4
+SyntaxError: invalid decimal literal
+city="vij"
+print(city)
+vij
+name="emmanuel"
+print(name)
+emmanuel
+>>> colour="black"
+>>> print(colour)
+black
+>>> print("color")
+color
+>>> first_name="emmanuel"
+>>> print(first_name)
+emmanuel
+>>> @a=100
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> _a=20
+>>> print(_a)
+20
+>>>  b=59
+...  
+SyntaxError: unexpected indent
+>>> email=emmanuelrajukoppisetti@gmail.com
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    email=emmanuelrajukoppisetti@gmail.com
+NameError: name 'emmanuelrajukoppisetti' is not defined
+>>> email="emmanuelrajukoppisetti@gmail.com"
+>>> print=email
+>>> a,b=3,4
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    print(a+b)
+TypeError: 'str' object is not callable
+>>> a,b=3,4
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    print(a+b)
+TypeError: 'str' object is not callable
+>>> print(email)
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    print(email)
+TypeError: 'str' object is not callable
+>>> fname,lname="emmanuel","raju"
+>>> print(fname,lname)
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    print(fname,lname)
+TypeError: 'str' object is not callable
