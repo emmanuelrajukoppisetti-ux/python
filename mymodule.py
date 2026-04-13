316727d11eea886843ad89079df93083ce81757c
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:13:25 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,25 @@
+#Module
+'''def greetings(name):
+    print("Welcome",name)'''
+
+
+'''a=3
+b=8
+print("the sum is",a+b)'''
+
+Details={"idnos":[10,20,30],
+         "names":["mahesh","aditya","emmanuel"],
+         "cities":["vij","hyd","rjy"]}
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    #a.append("code")
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this program is run as script")
+    else:
+        print("this progran is run as module")
+dummy()'''
