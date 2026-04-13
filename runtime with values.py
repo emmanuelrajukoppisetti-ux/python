fbbf4956cef419f255dec952afbfb32c601b4ba4
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:15:59 +0530

    Add files via upload

---
+++ runtime with values.py
@@ -0,0 +1,58 @@
+
+#maps topic contineous
+'''a=input('data1')
+b=input('data2')
+print(a+b)'''
+
+'''a,b=input('enter the name').split(',')
+print(a+b)'''
+
+'''a,b=[x for x in input('enter the names').split(',')]
+print(a+b)'''
+
+'''a=int(input('a value'))
+b=int(input('b value'))
+print(a+b)'''
+
+'''a,b=int(input('enter the values')).split(',')
+print(a+b)'''#value error
+
+'''a,b=[int(x) for x in input('enter the values').split(',')]
+print(a+b)'''
+
+
+'''a,b=map(int,input('enter the values').split(','))
+print(a+b)'''
+
+'''a=list(map(int,input('enter the values').split(',')))
+print(a)'''
+
+'''a=tuple(map(int,input('enter the values').split(',')))
+print(a)'''
+
+'''a=set(map(int,input('enter the values').split(',')))
+print(a)'''
+
+'''a=input('enter the key and value pairs')
+b=dict(i.split(':') for i in a.split(','))
+print(b)'''
+
+
+'''a=list(map(str,input('enter the values').split(',')))
+print(a)'''
+
+'''a=list(map(eval,input('enter the values').split(',')))
+print(a)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
