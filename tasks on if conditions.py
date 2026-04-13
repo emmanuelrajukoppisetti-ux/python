bcf5ca4159d0073f0fab370f5c6e4644dce1fe4f
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:14:18 +0530

    Add files via upload

---
+++ tasks on if conditions.py
@@ -0,0 +1,91 @@
+#Tasks
+#Voting
+'''age=int(input('Enter the age'))
+if age>=18 :
+    print('eligible for vote')
+else:
+    print('not eligible for vote')'''
+
+
+#Even or Odd
+'''num=int(input('enter a numder'))
+if num%2==0 :
+    print('it is even')
+else:
+    print('it is odd')'''
+
+#leap year
+'''year=int(input('enter the year'))
+if year%4==0 :
+    print('it is leap year')
+else:
+    print('it is not leap year')'''
+
+#vowels
+'''letters=['a','e','i','o','u']
+vowels=input('enter letter').lower()
+if vowels in letters:
+    print('it is vowel')
+else:
+    print('it is consonent')'''
+
+
+#guest code
+'''a=input('enter name')
+name='emmanuel'
+if a==name:
+    print('welcome emmanuel')
+else:
+    print('welcome guest')'''
+
+'''a=input('enter the name').lower()
+name=['emmanuel','sasi','mahesh','kevin','pavan']
+if a in name:
+    print('welcome',a)
+else:
+    print('welcome guest')'''
+
+#social media login
+
+
+'''username=input('username')
+password=int(input('password'))
+a='raju'
+b=1234
+if username==a:
+    if password==b:
+        print('login successful')
+else:
+    print('invalid credentials')'''
+
+
+'''username=input('username')
+password=int(input('password'))
+if username=='raju':
+    if password==1234:
+        print('login successful')
+    else:
+        print('incorrect passwoard')
+else:
+    print('invalid credentials')'''
+
+
+'''username=input('username')
+password=int(input('password'))
+if username=='raju' and password==1234 :
+        print('login successful')
+else:
+    print('invalid credential')'''
+
+#bakery
+'''cake_price=int(input('enter prize'))
+if cake_price==1200:
+    print('redvelvet cake')
+elif cake_price==1000:
+    print('chocalate cake')
+elif cake_price==800:
+    print('butter scotch cake')
+elif cake_price==600:
+    print('choco amond cake')
+else:
+    print('sorry cake is not available')'''
