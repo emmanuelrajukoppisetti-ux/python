34f8dac571583de994155aad83abb16f4f01f1e6
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:12:29 +0530

    Add files via upload

---
+++ multiple if and nested if.py
@@ -0,0 +1,131 @@
+#multiple if-conditionds
+'''a=5
+b=10
+if a<b:
+    print('less')
+if b>a:
+    print('greater')
+if a!=b:
+    print('not equal')'''
+
+
+'''a=5
+b=10
+if a<b:
+    print('less')
+elif b>a:
+    print('greater')
+elif a!=b:
+    print('not equal')'''
+
+a=5
+b=10
+if a==b:
+    print('less')
+if b>a:
+    print('greater')
+if a!=b:
+    print('not equal')
+
+
+'''a=5
+b=10
+if a<b:
+    print('less')
+if b>a:
+    print('greater')
+if a==b:
+    print('not equal')
+elif a!=b:
+    print('true')'''
+
+
+#multiple if-condition using logical operator
+'''a=3
+b=6
+if a<b and b>a:
+    print('true')
+if a!=b or a==b:
+    print('false')
+if not a<=b:
+    print('less')'''
+
+#multiple if-condition using identify operator
+'''a=6
+if type(a) is int:
+    print('it is integer')
+if type(a) is not float:
+    print('true')'''
+
+#multiple if-conditon using membership operator
+'''a=[2,3,4,5,6,7,8,9,10]
+if 3 in a:
+    print('true')
+if 10 not in a:
+    print('false')'''
+
+
+#Nested-if condition
+'''a=10
+b=20
+if a<b:
+    print('less')
+    if b>a:
+        print('greater')'''
+
+'''a=10
+b=20
+if a==b:
+    print('less')
+    if b>a:
+        print('greater')'''
+
+'''a=10
+b=20
+if a==b:
+    print('less')
+if b>a:
+    print('greater')'''
+
+        
+'''a=10
+b=20
+if a<=b:
+    print('less')
+    if b==a:
+        print('greater')
+    else:
+        print('true')'''
+        
+    
+'''a=10
+b=20
+if a==b:
+    print('less')
+    if b==a:
+        print('greater')
+else:
+    print('true')'''
+
+'''a=10
+b=20
+if a<=b:
+    print('less')
+    if b==a:
+        print('greater')
+    else:
+        print('true')
+else:
+    print('false')'''
+
+
+'''a=10
+b=20
+if a<=b:
+    print('less')
+    if b==a:
+        print('greater')
+    elif a==b:
+        print('not equal')
+    else:
+        print('false')'''
