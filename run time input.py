e8e3a4e1b23e3aff69d4960f37ad2441d34508bf
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:15:11 +0530

    Add files via upload

---
+++ run time input.py
@@ -0,0 +1,114 @@
+'''a=4
+b=6
+print(a+b)'''
+
+'''a=2
+b=3
+c=a+b
+print(c)'''
+
+#print(4=5)
+
+#run_time input()
+'''a=int(input('enter a value :'))
+b=int(input('enter b value :'))
+print(a+b)'''
+
+
+'''a=float(input('enter a value :'))
+b=float(input('enter b value :'))
+print(a+b)'''
+
+
+'''a=str(input('data 2'))
+b=str(input('data 1'))
+print(a+b)'''
+
+
+'''a=input('data 2')
+b=input('data 1')
+print(a+b)'''
+
+'''city=input('city name')
+print(city)'''
+
+
+'''a=complex(input('data 1:'))
+b=complex(input('data 2:'))
+print(a+b)'''
+
+
+'''a=bool(input('enter data 1'))
+b=bool(input('enter data 2'))
+c=bool(input('enter data 3'))
+print(a+b+c)'''
+
+
+'''a=int(input('a value'))
+b=int(input('b value'))
+option=int(input('choose the option 1.add 2.sub 3.mul '))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input('a value'))
+b=int(input('b value'))
+option=int(input(choose the option
+                 1.add
+                 2.sub
+                 3.mul ))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input('a value'))
+b=int(input('b value'))
+option=input('choose the option add sub mul ')
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+
+'''a=input()
+print(a)'''
+
+#swaping of two varables
+'''a=10
+b=20
+a,b=b,a
+print('the value of a is',a)
+print('the value of b is',b)'''
+
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print('the value of a is',a)
+print('the value of b is',b)'''
+
+
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print('the value of a is',a)
+print('the value of b is',b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print('the value of a=%d,b=%d'%(a,b))'''
+
+s
