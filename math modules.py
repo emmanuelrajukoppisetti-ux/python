e431be29c876cab242f760a3855e3a37da095c38
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:12:07 +0530

    Add files via upload

---
+++ math modules.py
@@ -0,0 +1,41 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#math module
+import math
+math.pi
+3.141592653589793
+math.pi*3
+9.42477796076938
+math.sqrt(2)
+1.4142135623730951
+>>> math.pow(2,2)
+4.0
+>>> math.log(10)
+2.302585092994046
+>>> math.sin(60)
+-0.3048106211022167
+>>> cos(30)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    cos(30)
+NameError: name 'cos' is not defined
+>>> math.cos(30)
+0.15425144988758405
+>>> math.tan(45)
+1.6197751905438615
+>>> math.ceil(2.7)
+3
+>>> math.ceil(5.9)
+6
+>>> math.floor(3.8)
+3
+>>> 
+>>> from math import pi,sqrt,log,pow
+>>> pi
+3.141592653589793
+>>> sqrt(4)
+2.0
+>>> log(4)
+1.3862943611198906
+>>> pow(4,5)
+1024.0
